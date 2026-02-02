525b96fde8785e3c5408caa61bc5c51a771e84ec
First <abaminsur@gmail.com>
2026-02-02 18:29:56 +0500

    project

--- src/main/java/Service/AnimalService.java
+++ src/main/java/Service/AnimalService.java
@@ -4,16 +4,28 @@ import models.Animal;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.AnimalRepository;
+
+import java.util.List;
+
+@Service
 public class AnimalService {
+
+    @Autowired
+    private AnimalRepository animalRepository; // Внедряем "руки", которые работают с БД
+
     public List<Animal> getAllAnimals() {
-        return List.of();
+        return animalRepository.findAll(); // Теперь возвращаем не пустой список, а данные из БД
     }
 
     public Animal saveAnimal(Animal animal) {
-        return animal;
+        return animalRepository.save(animal);
     }
 
     public void deleteAnimal(int id) {
+        animalRepository.deleteById(id);
     }
 
     public Animal assignKeeperToAnimal(int animalId, int keeperId) {
--- src/main/java/repositories/AnimalRepository.java
+++ src/main/java/repositories/AnimalRepository.java
@@ -30,4 +30,32 @@ public class AnimalRepository {
         }
         return animals;
     }
+
+    public Animal save(Animal animal) {
+        String sql = "INSERT INTO animals (name, species, age) VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement st = conn.prepareStatement(sql)) {
+            st.setString(1, animal.getName());
+            st.setString(2, animal.getSpecies());
+            st.setInt(3, animal.getId());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return animal;
+    }
+
+    public void deleteById(int id) {
+        String sql = "DELETE FROM animals WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement st = conn.prepareStatement(sql)) {
+
+            st.setInt(1, id);
+            st.executeUpdate();
+            System.out.println("Animal with id " + id + " was deleted.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
